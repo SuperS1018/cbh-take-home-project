db455f891c31eadd7d47d19a2a12f205e8b7c4d8
SuperS1018 <samchao1018@gmail.com>
2023-04-21 22:32:45 -0700

    Refectoring and explaination

--- dpk.js
+++ dpk.js
@@ -1,18 +1,36 @@
+/** Refactoring Explaination                                           */
+/** I move contants to the top so it will be easier to read.           */
+/** hashEncoding function should be stand alone because it             */
+/** will be easy to test if encoding is working.                       */
+/** getCandidateByEvent is also for the ease of testing.               */
+/** Now we can see all candidate is on the deterministicPartitionKey.  */
+
+
 const crypto = require("crypto");
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+const HASH_ALGORITHM = "sha3-512";
+const HASH_DIGEST_ENCODING = "hex"
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
+exports.hashEncoding = (data) => {
+  return crypto.createHash(HASH_ALGORITHM).update(data).digest(HASH_DIGEST_ENCODING);
+}
+
+exports.getCandidateByEvent = (event) => {
   let candidate;
-
   if (event) {
     if (event.partitionKey) {
       candidate = event.partitionKey;
     } else {
       const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+      candidate = this.hashEncoding(data);
     }
   }
+  return candidate;
+}
+
+exports.deterministicPartitionKey = (event) => {
+  let candidate = this.getCandidateByEvent(event);
 
   if (candidate) {
     if (typeof candidate !== "string") {
@@ -21,8 +39,9 @@ exports.deterministicPartitionKey = (event) => {
   } else {
     candidate = TRIVIAL_PARTITION_KEY;
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = this.hashEncoding(candidate);
   }
   return candidate;
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,52 @@
-const { deterministicPartitionKey } = require("./dpk");
+const { 
+  deterministicPartitionKey,
+  hashEncoding,
+  getCandidateByEvent
+} = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
+  const MOCK_EVENT_WITH_PARTITIONKEY = {
+    partitionKey: '20'
+  }
+
+  const MOCK_EVENT_WITHOUT_PARTITIONKEY = {
+    name: 'mock_name'
+  }
+
+  const MOCK_HASH_CODE_RESPONSE = "f01ce96c6e76e2602fdb6849b611c9c65cdd49371a1d3e6205819ee55f07a4c75090f2c0f486d4690f7590242d60f839709804659be5600d87a881886889bb19";
+
+  it("Should returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Should return 'partitionKey' when given an event with partitionKey", () => {
+    const trivialKey = deterministicPartitionKey(MOCK_EVENT_WITH_PARTITIONKEY);
+    expect(trivialKey).toBe("20");
+  });
+
+  it("Should return a hash code when given an event without partitionKey", () => {
+    const trivialKey = deterministicPartitionKey(MOCK_EVENT_WITHOUT_PARTITIONKEY);
+    expect(trivialKey).toBe(MOCK_HASH_CODE_RESPONSE);
+  });
+
+  describe("hashEncoding", () => {
+    it("Should convert data into hash codes", () => {
+      const data = JSON.stringify(MOCK_EVENT_WITHOUT_PARTITIONKEY)
+      const hash = hashEncoding(data);
+      expect(hash).toBe(MOCK_HASH_CODE_RESPONSE)
+    });
+  });
+
+  describe("getCandidateByEvent", () => {
+    it("Should get candidate by event with partitionKey", () => {
+      const candidate = getCandidateByEvent(MOCK_EVENT_WITH_PARTITIONKEY);
+      expect(candidate).toBe("20")
+    });
+
+    it("Should get candidate by event with partitionKey", () => {
+      const candidate = getCandidateByEvent(MOCK_EVENT_WITHOUT_PARTITIONKEY);
+      expect(candidate).toBe(MOCK_HASH_CODE_RESPONSE)
+    });
+  })
 });
